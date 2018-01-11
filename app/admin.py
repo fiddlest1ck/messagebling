3026073bf5c3195f1bd5a510fd2fca43bc2350ab
maciek <maciej.kuzma@cloudware.pl>
2018-01-11 14:46:33 +0100

    backend v3

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from app.models import Message, Attachment
 
 # Register your models here.
+class MessageAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Message, MessageAdmin)
+
+class AttachmentAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Attachment, AttachmentAdmin)
--- app/models.py
+++ app/models.py
@@ -3,5 +3,12 @@ from django.db import models
 
 class Message(models.Model):
     subject = models.CharField(max_length=255, verbose_name='Temat')
-    attachment = models.ForeignKey('auth.User', on_delete='ignore')
+    users = models.ManyToManyField('auth.User')
+    attachments = models.ManyToManyField('app.Attachment')
     body = models.TextField()
+
+
+class Attachment(models.Model):
+    name = models.CharField(max_length=255)
+    file_object = models.FileField(upload_to='static/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
